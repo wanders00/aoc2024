fab887fbd476afbd07aeaeb596a0bf998058ebad
wanders00 <williama00@hotmail.se>
2024-12-06 13:59:50 +0100

    Day6: part 2 & README

---
+++ day06/README.md
@@ -0,0 +1,32 @@
+# Day 6
+
+Link to the problem on the Advent of Code website - [click me](https://adventofcode.com/2024/day/6)
+
+## Description
+
+Back to rust today, it was similar to the day4 problem where I used rust (lots of coordinates stuff), so I decided to use rust again cause I could peek at my previous code.
+
+Im happy with Part 1 solution, but Part 2 was a bit more challenging, so I ended up using a "brute force" solution, which is not the best, but it works. Only downside is that this is the first solution which takes more than 1 second to run 😨
+
+On a side note, I think I was too ambitious with having a in-depth description in each daily README.md, so from now on I will keep it short and sweet. I just cannot be bothered to write a lot of text after solving the problem 😅
+
+### Time elapsed
+
+- Part 1: 1.2992ms
+
+- Part 2: 1.7468661s
+
+## How to run
+
+### Prerequisites
+
+- Rust / Cargo installed
+
+### Run the code
+
+```bash
+1. Clone the repo               |   git clone
+2. Navigate into todays folder  |   cd day<x>
+3. Compile & run the code       |   cargo run
+```
+
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,14 +1,15 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::time::Instant;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Hash, Debug, Clone, Copy, PartialEq, Eq)]
 struct Position {
     y: isize,
     x: isize,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Hash, Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -16,51 +17,105 @@ enum Direction {
     Right,
 }
 
+static mut START_POS: Position = Position { y: -1, x: -1 };
+
+fn run_maze(input: &mut Vec<Vec<char>>, find_loop: bool) -> usize {
+    unsafe {
+        set_char_at_position(input, START_POS, 'X');
+    }
+    let mut pos;
+    unsafe {
+        pos = START_POS;
+    }
+    let mut dir = Direction::Up;
+    let mut next_pos = move_position(pos, dir);
+    let mut visited_positions: HashSet<(Position, Direction)> = HashSet::new();
+
+    while pos.y >= 0
+        && pos.y < input.len() as isize
+        && pos.x >= 0
+        && pos.x < input[pos.y as usize].len() as isize
+    {
+        if let Some(c) = get_char_at_position(&input, next_pos) {
+            if c == '#' {
+                if find_loop {
+                    let tuple = (pos, dir);
+                    if visited_positions.contains(&tuple) {
+                        return 1;
+                    }
+                    visited_positions.insert(tuple);
+                }
+                dir = turn_right(dir);
+                next_pos = move_position(pos, dir);
+            } else {
+                pos = next_pos;
+                set_char_at_position(input, pos, 'X');
+                next_pos = move_position(pos, dir);
+            }
+        } else {
+            break;
+        }
+    }
+
+    if find_loop {
+        return 0;
+    }
+
+    let result = count_char_in_vec(&input, 'X');
+    println!("Result: {}", result);
+    return result;
+}
+
+fn part_2(input: &mut Vec<Vec<char>>) {
+    let mut count = 0;
+    for y in 0..input.len() {
+        for x in 0..input[y].len() {
+            let pos = Position {
+                y: y as isize,
+                x: x as isize,
+            };
+            if get_char_at_position(input, pos) == Some('X') {
+                let mut temp = input.clone();
+                set_char_at_position(&mut temp, pos, '#');
+                count += run_maze(&mut temp, true);
+            }
+        }
+    }
+
+    println!("Result p2: {}", count);
+}
+
 fn main() {
     let filename = "src/input.txt";
     let start = Instant::now();
     match read_file(filename) {
         Ok(mut input) => {
-            let start_pos = find_start_position(&input);
-            set_char_at_position(&mut input, start_pos, 'X');
-            let mut pos = start_pos;
-            let mut dir = Direction::Up;
-            let mut next_pos = move_position(pos, dir);
-
-            while pos.y >= 0
-                && pos.y < input.len() as isize
-                && pos.x >= 0
-                && pos.x < input[pos.y as usize].len() as isize
-            {
-                if let Some(c) = get_char_at_position(&input, next_pos) {
-                    if c == '#' {
-                        dir = turn_right(dir);
-                        next_pos = move_position(pos, dir);
-                    } else {
-                        pos = next_pos;
-                        set_char_at_position(&mut input, pos, 'X');
-                        next_pos = move_position(pos, dir);
-                    }
-                } else {
-                    println!("The Guard found their way out at {}.{}", pos.y, pos.x);
-                    break;
-                }
+            unsafe {
+                START_POS = find_start_position(&input);
             }
+            run_maze(&mut input, false);
+            let duration = start.elapsed();
+            println!("Time elapsed - Part 1: {:?}", duration);
 
-            println!("Result: {}", count_char_in_vec(&input, 'X'));
+            let start = Instant::now();
+            part_2(&mut input);
+            let duration = start.elapsed();
+            println!("Time elapsed - Part 2: {:?}", duration);
         }
         Err(e) => eprintln!("Error: {}", e),
     }
-    let duration = start.elapsed();
-    println!("Time elapsed - Part 1: {:?}", duration);
 }
 
 fn count_char_in_vec(input: &Vec<Vec<char>>, target: char) -> usize {
-    input.iter().flat_map(|row| row.iter()).filter(|&&c| c == target).count()
+    input
+        .iter()
+        .flat_map(|row| row.iter())
+        .filter(|&&c| c == target)
+        .count()
 }
 
 fn get_char_at_position(input: &Vec<Vec<char>>, pos: Position) -> Option<char> {
-    if pos.y < input.len() as isize && pos.x < input[pos.y as usize].len() as isize {
+    if pos.y >= 0 && pos.y < input.len() as isize && pos.x >= 0 && pos.x < input[pos.y as usize].len() as isize {
         Some(input[pos.y as usize][pos.x as usize])
     } else {
         None
@@ -68,7 +123,7 @@ fn get_char_at_position(input: &Vec<Vec<char>>, pos: Position) -> Option<char> {
 }
 
 fn set_char_at_position(input: &mut Vec<Vec<char>>, pos: Position, c: char) {
-    if pos.y < input.len() as isize && pos.x < input[pos.y as usize].len() as isize {
+    if pos.y >= 0 && pos.y < input.len() as isize && pos.x >= 0 && pos.x < input[pos.y as usize].len() as isize {
         input[pos.y as usize][pos.x as usize] = c;
     }
 }
